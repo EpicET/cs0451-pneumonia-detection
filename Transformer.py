1c16915c94c75bca7dfbfedb6688e80652277008
EpicET <etowner@middlebury.edu>
2025-04-20 20:04:59 -0400

    intialized neural network

---
+++ Transformer.py
@@ -0,0 +1,13 @@
+import torch
+
+class Transformer():
+    
+    def __init__(self):
+        super(Transformer, self).__init__()
+        
+    def forward(self, x):
+        pass
+        
+  
+    
+    
