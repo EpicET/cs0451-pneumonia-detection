ffa23a70c2fda07ae71b5f957e3bd648993f5ca2
EpicET <etowner@middlebury.edu>
2025-04-20 22:59:21 -0400

    Began layout of transformer

--- Transformer.py
+++ Transformer.py
@@ -2,8 +2,23 @@ import torch
 
 class Transformer():
     
-    def __init__(self):
+    def __init__(self, input_dim, output_dim, num_heads, num_layers):
         super(Transformer, self).__init__()
+        
+        # Positional Encoding
+        
+        
+        # Encoding Layers
+        
+        ## Multi-Head Attention
+        
+        ## Normalization
+        
+        ## Feed Forward Network
+        
+        # Decoding Layers
+        
+        
         
     def forward(self, x):
         pass
